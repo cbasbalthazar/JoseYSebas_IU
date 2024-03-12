71d66e595201a60b6645ed017e2e8f31c989d7cc
cbasbalthazar <163176821+cbasbalthazar@users.noreply.github.com>
2024-03-12 14:08:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
+import streamlit as st
+from PIL import Image
 
+st.tittle ("Ciudades inteligentes")
+
+st.header ("Este es un espacio para aprender sobre las ciudades inteligentes y su importancia")
+
+image = image.open("Barcelona int.png")
