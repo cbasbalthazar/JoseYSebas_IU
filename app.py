4cae7b83c871e163c0fbc708d2d270403972baef
cbasbalthazar <163176821+cbasbalthazar@users.noreply.github.com>
2024-03-12 14:17:22 -0500

    app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from PIL import Image
 
-st.tittle ("Ciudades inteligentes")
+st.tittle("Ciudades inteligentes")
 
-st.header ("Este es un espacio para aprender sobre las ciudades inteligentes y su importancia")
+st.header("Este es un espacio para aprender sobre las ciudades inteligentes y su importancia")
 
-image = image.open("Barcelona int.png")
-st.image (image, caption = "Conexión IOT en barcelona")
-st.write ("Hablar de inteligencia urbana es conectar a los ciudadanos con el entorno virtual para mejorar su calidad de vida")
-st.write ("Ingresa al link para visualizar estos datos de la ciudad [lINK] (https://demo.thingsboard.io/dashboards/e2eb9330-e0a1-11ee-bc03-55147b89654f)")
+image=image.open("Barcelona int.png")
+st.image(image, caption = "Conexión IOT en barcelona")
+st.write("Hablar de inteligencia urbana es conectar a los ciudadanos con el entorno virtual para mejorar su calidad de vida")
+st.write("Ingresa al link para visualizar estos datos de la ciudad [lINK] (https://demo.thingsboard.io/dashboards/e2eb9330-e0a1-11ee-bc03-55147b89654f)")
