36c52a5edf687b1bb277e6361b6dd20a94533a7b
cbasbalthazar <163176821+cbasbalthazar@users.noreply.github.com>
2024-05-14 13:42:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("Ciudades inteligentes")
 
 st.header("Este es un espacio para aprender sobre las ciudades inteligentes y su importancia")
 
-image=image.open("smart cities.jpg")
+image = Image.open("smart cities.jpg")
 st.image(image, caption = "Conexión IOT en barcelona")
 st.write("Hablar de inteligencia urbana es conectar a los ciudadanos con el entorno virtual para mejorar su calidad de vida")
 st.write("Ingresa al link para visualizar estos datos de la ciudad [lINK] (https://demo.thingsboard.io/dashboards/e2eb9330-e0a1-11ee-bc03-55147b89654f)")
